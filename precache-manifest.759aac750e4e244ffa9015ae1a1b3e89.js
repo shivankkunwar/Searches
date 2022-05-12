5d76106ab469dd8a8d855f867ed41ba33008b9bd
shivank <shivankkunwar100@gmail.com>
2022-05-13 00:02:46 +0530

    Updates

---
+++ precache-manifest.759aac750e4e244ffa9015ae1a1b3e89.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "877bb50b47da87d4c4ad",
+    "url": "/Searches/static/css/main.9a140bf7.chunk.css"
+  },
+  {
+    "revision": "877bb50b47da87d4c4ad",
+    "url": "/Searches/static/js/main.877bb50b.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Searches/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "42dc3d5284287893fef9",
+    "url": "/Searches/static/css/2.fa0966ae.chunk.css"
+  },
+  {
+    "revision": "42dc3d5284287893fef9",
+    "url": "/Searches/static/js/2.42dc3d52.chunk.js"
+  },
+  {
+    "revision": "dea511baf853f49df03b",
+    "url": "/Searches/static/js/runtime~main.dea511ba.js"
+  },
+  {
+    "revision": "8daa6603d78efc3fa6eb585ff7356923",
+    "url": "/Searches/static/media/Nillota.8daa6603.ttf"
+  },
+  {
+    "revision": "fd34c98fb489b63d68686ca3cc5b8620",
+    "url": "/Searches/index.html"
+  }
+];
