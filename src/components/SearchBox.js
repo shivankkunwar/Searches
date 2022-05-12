089ce4df352b86cf9f09e6891ee960df60cc7ac8
shivank <shivankkunwar100@gmail.com>
2022-05-12 23:53:07 +0530

    Hooks used :changed to functional Components

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,6 +1,7 @@
 import React from "react";
 const SearchBox=({searchField,searchChange})=>{
-    return(<div className="pa2">
+    return(
+    <div className="pa2">
     <input 
     className="pa3 ba b--green bg-lightest-blue"
     type='search'
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,54 +1,53 @@
-import React,{ Component } from 'react';
+import React,{ useState,  useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';                                                                                                                                                                                                                    
 import Scroll from  '../components/Scroll';
 // import { peoples } from './peoples';
 import './App.css';
- class App extends Component{
-     constructor(){
-         super()
-         this.state={
-            peoples:[],
-            searchField:''
-         }
-     }
-     onSearchChange = (event) =>{
-        this.setState({searchField: event.target.value });
+ function App (){
+ 
+
+    const [peoples,setPeoples]=useState([]);
+    const [searchField,setSearchField]=useState("");
+    // const [count,setCount]= useState(0);
+     
+     const onSearchChange = (event) =>{
+        setSearchField(event.target.value);
         
-            
         
      }
-     componentDidMount(){
+     useEffect(()=>{
+   
          fetch('https://jsonplaceholder.typicode.com/users')
          .then(response=>response.json())
-         .then(users=>this.setState({peoples:users}));
-      
-     }
-   render(){ 
-       const {peoples,searchField}= this.state;
+         .then(users=>{setPeoples(users)});
+        // console.log(count);
+     },[]);
+    //  },[count]);
+ 
     const filteredPeople = peoples.filter(people=>
         {
+           
             return people.name.toLowerCase().includes(searchField.toLowerCase());
         }
         
         );
-    if(peoples.length===0){
-        return <h1 className='tc'>LOADING</h1>
-    }  
-    else{
-        return (
-        <div className='tc'>
+        
+
+        return !peoples.length?
+        <h1 className='tc'>LOADING</h1>:
+        (<div className='tc'>
+
         <h1 className="f1 lh-title ">SEARCHES..</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
+        {/* <button onClick={()=>{setCount(count+ 1)}}>Click HERE</button> */}
+        <SearchBox searchChange={onSearchChange}/>
         <Scroll>
         <CardList peoples={filteredPeople}/>
         </Scroll>
         
-        </div>
-        
-    );
-    }  
+        </div>)
+   
       
-    }
+    
 }
 export default App;
